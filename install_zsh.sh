bebae522e48511082a1c0c59c59d2b6ef81ad7fd
Alex Lourie <djay.il@gmail.com>
2022-08-02 23:05:32 +0930

    Fix installer
    Signed-off-by: Alex Lourie <djay.il@gmail.com>

--- install_zsh.sh
+++ install_zsh.sh
@@ -2,7 +2,9 @@
 if command -v apt > /dev/null; then
   sudo apt install git curl zsh
 fi
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh > install_omzsh.sh
+chmod +x install_omzsh.sh
+./install_omzsh.sh -y
 mv ~/.zshrc ~/.zshrc-omz
 wget bit.ly/alourie-zshrc -O .zshrc
 zsh
