7fa80b138b59d28884dabbb3985ef3f273b054ed
Alex Lourie <djay.il@gmail.com>
2022-08-03 12:28:10 +0930

    handle unattended install
    Signed-off-by: Alex Lourie <djay.il@gmail.com>

--- install_zsh.sh
+++ install_zsh.sh
@@ -4,7 +4,7 @@ if command -v apt > /dev/null; then
 fi
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh > install_omzsh.sh
 chmod +x install_omzsh.sh
-./install_omzsh.sh -y
+RUNZSH=no CHSH=yes ./install_omzsh.sh 
 mv ~/.zshrc ~/.zshrc-omz
 wget bit.ly/alourie-zshrc -O .zshrc
 zsh
